e5c33689edfc70213cc83a04b8a3f56e64c6546a
Simon Exley <21612382@sun.ac.za>
2022-11-02 21:56:37 +0200

    11.38

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,22 @@
+import { Box, Container, Heading } from '@chakra-ui/react'
+
 const Page = ( ) => {
+
     return(
-        <div>Hello</div>
+        <Container>
+            <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+                Hello, I&apos;m a Final Year Computer Science Student based in South Africa !
+            </Box>
+
+            <Box display={{ md: 'flex' }}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Simon Exley
+                    </Heading>
+                    <p>A Cyber Security & Software Development enthusiast </p>
+                </Box>
+            </Box>
+        </Container>
     )    
     
 }
